5b69d6e68e74a77dfc9cf3964db3f2ef4a0c0c93
Richard <rail.rich@gmail.com>
2017-04-22 00:59:48 -0400

    Expllose moi cA !

--- Assets/Scripts/Astetics/PlatnetShatter/PlatnetShatter.cs
+++ Assets/Scripts/Astetics/PlatnetShatter/PlatnetShatter.cs
@@ -3,10 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlatnetShatter : MonoBehaviour {
-public PlatnetShatterChunk[] ShaderedPrefabs;
-
-	public int ShatterAmountMin;
-	public int ShatterAmountMax;
+	
+	public ParticleSystem ExplosionParticleSystem;
 
 	public bool testDestroy;	
 	void Start () {
@@ -21,24 +19,9 @@ public PlatnetShatterChunk[] ShaderedPrefabs;
 	}
     public void Shatter()
     {
-		int amount =  Random.Range(ShatterAmountMin,ShatterAmountMax);
-		for (int i = 0; i < amount; i++)
-		{
-			SpawnPiece();
-		}
+		ExplosionParticleSystem.Play();
 
         Destroy(this.gameObject);
-    }
-
-    private void SpawnPiece()
-    {
-		var newPiece = GameObject.Instantiate(ShaderedPrefabs[Random.Range(0,ShaderedPrefabs.Length-1)]);
-		var newPieceGo = newPiece.gameObject;
-
-		Vector3 randomDirection = new Vector3(Random.value, Random.value, Random.value);
-		newPiece.Velocity = randomDirection * Random.Range(40,90);
-		newPiece.spin = Random.Range(-60f,60f);
-		
     }
 
 }
