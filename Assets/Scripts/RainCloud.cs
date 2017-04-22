2be1c07e577b820e67731fb5156f0c43fcb16680
Alex Massé <masse.alex@gmail.com>
2017-04-22 01:45:57 -0400

    rain drops

--- Assets/Scripts/RainCloud.cs
+++ Assets/Scripts/RainCloud.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class RainCloud : MonoBehaviour {
 
     public float distanceToGround;
+    public GameObject rainDropPrefab;
+    public float rainZoneWidth;
+
+    private float lastTimeSpawn;
 
 	// Use this for initialization
 	void Start () {
@@ -16,10 +20,20 @@ public class RainCloud : MonoBehaviour {
         transform.localEulerAngles = new Vector3(0, 0, angle - 90);
 
         transform.position = (Vector3)hit.point + transform.up * distanceToGround;
+
+        lastTimeSpawn = Time.time;
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(Time.time - lastTimeSpawn > 1)
+        {
+            lastTimeSpawn = Time.time;
+            GameObject obj = Instantiate(rainDropPrefab);
+            obj.transform.position = transform.position;
+            obj.transform.up = transform.up;
+
+            obj.transform.position += transform.right * Random.Range(-rainZoneWidth * 0.5f, rainZoneWidth * 0.5f);
+        }
 	}
 }
---
+++ Assets/Scripts/RainDrop.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainDrop : MonoBehaviour {
+
+    public LayerMask PlanetLayer;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position -= transform.up * Time.deltaTime;
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(PlanetLayer == (PlanetLayer | (1 << collision.gameObject.layer)))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
