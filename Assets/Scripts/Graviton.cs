166f6cb214facd310adee301c07ae1b96c30729f
Magicolo <straty91@hotmail.com>
2017-04-22 03:52:28 -0400

    + de volcan

---
+++ Assets/Scripts/Graviton.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Graviton : MonoBehaviour
+{
+	public static Graviton Instance { get; private set; }
+
+	public Vector2 Gravity = new Vector2(0f, -9.8f);
+
+	void Awake()
+	{
+		Instance = this;
+	}
+}
---
+++ Assets/Scripts/Lava.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Lava : MonoBehaviour
+{
+	public LavaParticle Prefab;
+	public Vector2 SpawnDelay = new Vector2(0.25f, 1f);
+	public Vector2 Burst = new Vector2(1f, 5f);
+
+	float nextParticle;
+	readonly Stack<LavaParticle> pool = new Stack<LavaParticle>();
+
+	void FixedUpdate()
+	{
+		while (Chronos.Instance.CurrentTime >= nextParticle)
+		{
+			var burst = (int)Random.Range(Burst.x, Burst.y);
+
+			for (int i = 0; i < burst; i++)
+			{
+				nextParticle += Random.Range(SpawnDelay.x, SpawnDelay.y);
+				Spawn();
+			}
+		}
+	}
+
+	public LavaParticle Spawn()
+	{
+		var particle = pool.Count > 0 ? pool.Pop() : Instantiate(Prefab, transform);
+		particle.gameObject.SetActive(true);
+		particle.Initialize(this, transform.position, new Vector2(Random.Range(-50f, 50f), Random.Range(25f, 100f)), 0.98f);
+
+		return particle;
+	}
+
+	public void Despawn(LavaParticle particle)
+	{
+		particle.gameObject.SetActive(false);
+		pool.Push(particle);
+	}
+}
