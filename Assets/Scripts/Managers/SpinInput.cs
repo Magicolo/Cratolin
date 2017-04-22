1ee470bc1d119f59cb69d1f5f4a7137631111f8e
Richard <rail.rich@gmail.com>
2017-04-22 18:54:07 -0400

    Spin start

---
+++ Assets/Scripts/Managers/SpinInput.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinInput : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var mouseP = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		//Vector3.
+	}
+}
