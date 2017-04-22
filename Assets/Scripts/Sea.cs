5484ca3f9bed77ddf382c10032dce5c6628368ac
Alex Massé <masse.alex@gmail.com>
2017-04-22 12:23:22 -0400

    sea spawning trees.

--- Assets/Scripts/Sea.cs
+++ Assets/Scripts/Sea.cs
@@ -6,11 +6,18 @@ public class Sea : MonoBehaviour {
 
     public float increment;
     public SpriteRenderer sprite;
+    public Tree treePrefab;
+    public float timeBetweenTreeSpawn;
+    public int maxTreeCountEachSide;
+    public LayerMask planetMask;
 
     private float ratio;
+    private float lastTimeSpawnedTree;
+    public List<Tree> lstTreesSpawnedLeft = new List<Tree>();
+    public List<Tree> lstTreesSpawnedRight = new List<Tree>();
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         ratio = 0;
         RefreshRatio();
 
@@ -22,6 +29,40 @@ public class Sea : MonoBehaviour {
             IncreaseWater();
         if (Input.GetKeyDown(KeyCode.P))
             ReduceWater();
+
+        if(ratio > 0.5f && Time.time - lastTimeSpawnedTree > timeBetweenTreeSpawn)
+        {
+            lastTimeSpawnedTree = Time.time;
+
+            bool spawnLeft = Random.Range(0, 2) == 0;
+            List<Tree> lstListToSpawnIn = null;
+            Vector3 spawnPos = transform.position;
+            if(spawnLeft && lstTreesSpawnedLeft.Count < maxTreeCountEachSide)
+            {
+                lstListToSpawnIn = lstTreesSpawnedLeft;
+                spawnPos = new Vector3(GetComponent<BoxCollider2D>().bounds.min.x, GetComponent<BoxCollider2D>().bounds.max.y);
+                spawnPos += -transform.right * Random.Range(40, 60) * lstListToSpawnIn.Count;
+            }
+            else if (!spawnLeft && lstTreesSpawnedRight.Count < maxTreeCountEachSide)
+            {
+                lstListToSpawnIn = lstTreesSpawnedRight;
+                spawnPos = new Vector3(GetComponent<BoxCollider2D>().bounds.max.x, GetComponent<BoxCollider2D>().bounds.max.y);
+                spawnPos += transform.right * Random.Range(40, 60) * lstListToSpawnIn.Count;
+            }
+
+            if(lstListToSpawnIn != null)
+            {
+                var direction = spawnPos.normalized;
+                var hit = Physics2D.Raycast(direction * 1000f, -direction, 1000f, planetMask);
+
+                if (hit)
+                {
+                    Tree newTree = Instantiate(treePrefab, hit.point, Quaternion.FromToRotation(Vector2.up, direction), Planet.Instance.transform);
+                    lstListToSpawnIn.Add(newTree);
+                }
+            }
+        }
+        
     }
 
     public void IncreaseWater()
