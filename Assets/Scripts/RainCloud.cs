c5b6781f996945a3b0a0979dcf5d12da3a5f5efe
Alex Massé <masse.alex@gmail.com>
2017-04-22 02:15:33 -0400

    power used un wheel menu

--- Assets/Scripts/RainCloud.cs
+++ Assets/Scripts/RainCloud.cs
@@ -12,15 +12,6 @@ public class RainCloud : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector3.zero - transform.position);
-
-        Vector2 v2 = (Vector2)transform.position - Vector2.zero;
-        float angle = Mathf.Atan2(v2.y, v2.x) * Mathf.Rad2Deg;
-
-        transform.localEulerAngles = new Vector3(0, 0, angle - 90);
-
-        transform.position = (Vector3)hit.point + transform.up * distanceToGround;
-
         lastTimeSpawn = Time.time;
     }
 	
---
+++ Assets/Scripts/RainPower.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainPower : PowerBase {
+
+    public LayerMask Mask;
+    public RainCloud Prefab;
+
+    public override GameObject Create(Vector2 position)
+    {
+        if (!CanPlace(position))
+            return null;
+
+        RainCloud objRainCloud = Instantiate(Prefab);
+
+        RaycastHit2D hit = Physics2D.Raycast(objRainCloud.transform.position, Vector3.zero - transform.position, 10000, Mask);
+
+        Vector2 v2 = (Vector2)transform.position - Vector2.zero;
+        float angle = Mathf.Atan2(v2.y, v2.x) * Mathf.Rad2Deg;
+
+        objRainCloud.transform.localEulerAngles = new Vector3(0, 0, angle - 90);
+
+        objRainCloud.transform.position = (Vector3)hit.point + objRainCloud.transform.up * objRainCloud.distanceToGround;
+
+        return objRainCloud.gameObject;
+    }
+
+    public override bool CanPlace(Vector2 position)
+    {
+        return true;
+    }
+}
