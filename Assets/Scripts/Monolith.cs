adfca6926cafc71314613e80fef40f0fad929cc0
Alex Massé <masse.alex@gmail.com>
2017-04-23 01:43:11 -0400

    working monolith and monekys.

---
+++ Assets/Scripts/Monolith.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monolith : MonoBehaviour {
+
+    public SpriteRenderer spriteRenderer;
+    public Sprite[] spriteSteps;
+    public Sprite completed;
+
+    public static Monolith Instance { get; private set; }
+
+    private bool hasEmerged = false;
+    private List<Walker> registeredWalker = new List<Walker>();
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		if(hasEmerged && spriteRenderer.transform.localPosition.y < 0)
+        {
+            spriteRenderer.transform.localPosition += new Vector3(0, Time.deltaTime * 4);
+
+            if (spriteRenderer.transform.localPosition.y > 0)
+                spriteRenderer.transform.localPosition = Vector3.zero;
+        }
+	}
+
+    public void Emerge()
+    {
+        if(!hasEmerged)
+        {
+            hasEmerged = true;
+        }
+    }
+
+    public bool HasRegisteredWalker(Walker walker)
+    {
+        return registeredWalker.Contains(walker);
+    }
+
+    public bool IsCompleted
+    {
+        get { return registeredWalker.Count >= spriteSteps.Length; }
+    }
+
+    public void RegisterWalker(Walker walker)
+    {
+        registeredWalker.Add(walker);
+
+        if (IsCompleted)
+            spriteRenderer.sprite = completed;
+        else
+            spriteRenderer.sprite = spriteSteps[registeredWalker.Count];
+    }
+
+    public float AttrackDistance
+    {
+        get { return (registeredWalker.Count + 1) * 25; }
+    }
+}
--- Assets/Scripts/Walker.cs
+++ Assets/Scripts/Walker.cs
@@ -7,25 +7,44 @@ public class Walker : MonoBehaviour {
     public float distanceToGround;
     public LayerMask WalkerCollision;
     public Transform Visual;
+    public GameObject walkBase;
+    public GameObject walkEvolved;
+    public GameObject scared;
+    public GameObject idle;
+    public GameObject lookAround;
 
     private bool isGoingRight = true;
     private float timeSinceLastFear = float.MinValue;
+    private float timeSpawn;
+    private bool isWalking = true;
 
-	// Use this for initialization
-	void Start () {
+    private float lookAroundTimer = 1000;
+
+    public bool IsEvolved { get { return Time.time - timeSpawn > 20; } }
+    public bool ReachedGoal { get; set; }
+
+    // Use this for initialization
+    void Start () {
         transform.parent = Planet.Instance.transform;
-	}
+        timeSpawn = Time.time;
+
+        lookAroundTimer = Random.Range(0f, 5f);
+
+    }
 
     public void Fear(Vector3 pPosition)
     {
-        timeSinceLastFear = Time.time;
+        if(!ReachedGoal)
+        {
+            timeSinceLastFear = Time.time;
 
-        bool fearLeftOf = (Vector2.Angle(transform.right, pPosition)) > 90 && (Vector2.Angle(transform.right, pPosition)) < 180;
+            bool fearLeftOf = (Vector2.Angle(transform.right, pPosition)) > 90 && (Vector2.Angle(transform.right, pPosition)) < 180;
 
-        if (fearLeftOf)
-            isGoingRight = false;
-        else
-            isGoingRight = true;
+            if (fearLeftOf)
+                isGoingRight = false;
+            else
+                isGoingRight = true;
+        }
     }
 
     public float CurrentSpeed
@@ -48,9 +67,13 @@ public class Walker : MonoBehaviour {
 	// Update is called once per frame
 	void LateUpdate ()
     {
+        isWalking = !ReachedGoal;
+
+        
+
         // stick to ground
         RaycastHit2D hit = Physics2D.Raycast(transform.position, -transform.up, 100000, WalkerCollision);
-        if(hit.collider != null)
+        if (hit.collider != null)
         {
             Collider2D col = hit.collider;
             transform.up = hit.normal;
@@ -58,26 +81,57 @@ public class Walker : MonoBehaviour {
         }
 
         // walk in current direction, left or right
-        RaycastHit2D hitLeftRight = Physics2D.Raycast(transform.position + transform.up * distanceToGround, transform.right, distanceToGround * 2, WalkerCollision);
-        bool canGoRight = (hitLeftRight.collider == null);
-        hitLeftRight = Physics2D.Raycast(transform.position + transform.up * distanceToGround, -transform.right, distanceToGround * 2, WalkerCollision);
-        bool canGoLeft = (hitLeftRight.collider == null);
+        if (isWalking)
+        {
+            
+
+            RaycastHit2D hitLeftRight = Physics2D.Raycast(transform.position + transform.up * distanceToGround, transform.right, distanceToGround * 2, WalkerCollision);
+            bool canGoRight = (hitLeftRight.collider == null);
+            hitLeftRight = Physics2D.Raycast(transform.position + transform.up * distanceToGround, -transform.right, distanceToGround * 2, WalkerCollision);
+            bool canGoLeft = (hitLeftRight.collider == null);
 
 
-        if (isGoingRight && canGoRight)
-            transform.localPosition += transform.right * Time.deltaTime * CurrentSpeed;
-        else if (isGoingRight && !canGoRight)
-            isGoingRight = false;
-        else if (!isGoingRight && canGoLeft)
-            transform.localPosition -= transform.right * Time.deltaTime * CurrentSpeed;
-        else if (!isGoingRight && !canGoLeft)
-            isGoingRight = true;
+            if (isGoingRight && canGoRight)
+                transform.localPosition += transform.right * Time.deltaTime * CurrentSpeed;
+            else if (isGoingRight && !canGoRight)
+                isGoingRight = false;
+            else if (!isGoingRight && canGoLeft)
+                transform.localPosition -= transform.right * Time.deltaTime * CurrentSpeed;
+            else if (!isGoingRight && !canGoLeft)
+                isGoingRight = true;
+        }
+        
 
         UpdateSprite();
+
+        if(IsEvolved)
+        {
+            Monolith.Instance.Emerge();
+
+            if(!ReachedGoal && !Monolith.Instance.IsCompleted && Vector2.Distance(Monolith.Instance.transform.position, transform.position) < Monolith.Instance.AttrackDistance)
+            {
+                ReachedGoal = true;
+                transform.parent = Planet.Instance.Root;
+                Monolith.Instance.RegisterWalker(this);
+            }
+        }
     }
 
     private void UpdateSprite()
     {
         Visual.transform.localScale = new Vector3((isGoingRight ? -1 : 1), 1, 1);
+
+        if (ReachedGoal)
+        {
+            lookAroundTimer -= Time.deltaTime;
+
+            if (lookAroundTimer < 0)
+                lookAroundTimer = Random.Range(4f, 10f);
+        }
+
+        walkBase.SetActive(!IsEvolved);
+        walkEvolved.SetActive(IsEvolved && isWalking);
+        idle.SetActive(IsEvolved && !isWalking && (!ReachedGoal || lookAroundTimer > 0.3f));
+        lookAround.SetActive(IsEvolved && !isWalking && (ReachedGoal && lookAroundTimer < 0.3f));
     }
 }
