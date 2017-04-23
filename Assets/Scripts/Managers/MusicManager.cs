cc5fc8d314b3a60d69efa764072bc63f71e48770
Alex Massé <masse.alex@gmail.com>
2017-04-23 17:09:48 -0400

    music integrated.

---
+++ Assets/Scripts/Managers/MusicManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<AudioSource>().pitch = Mathf.Clamp(Time.timeScale  * 0.2f, 1f, 100f);
+	}
+}
