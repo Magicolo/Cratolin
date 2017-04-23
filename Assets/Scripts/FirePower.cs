a14c330a0a09f5a757f0dee0cdc8889130b5411f
Magicolo <straty91@hotmail.com>
2017-04-23 00:24:41 -0400

    t'as toute brisé

--- Assets/Scripts/FirePower.cs
+++ Assets/Scripts/FirePower.cs
@@ -1,62 +1,55 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class FirePower : PowerBase {
+public class FirePower : PowerBase
+{
+	public float Cooldown;
+	public SpriteRenderer firePreview;
 
-    public float Cooldown;
-    public SpriteRenderer firePreview;
+	public override bool CanUse { get { return base.CanUse && Chronos.Instance.Time - lastUse >= Cooldown; } }
+	public override int RemainingUses { get { return -1; } }
+	public override PowerManager.Powers Power { get { return PowerManager.Powers.Fire; } }
 
-    public override bool CanUse
-    {
-        get { return base.CanUse && Chronos.Instance.Time - lastUse >= Cooldown; }
-    }
-    public override int RemainingUses
-    {
-        get { return -1; }
-    }
+	float lastUse = float.MinValue;
 
-    float lastUse = float.MinValue;
+	public override GameObject Create(Vector2 position)
+	{
+		if (!CanPlace(position))
+			return null;
 
-    public override GameObject Create(Vector2 position)
-    {
-        if (!CanPlace(position))
-            return null;
+		RaycastHit2D hit = Physics2D.Raycast(position, -position.normalized);
 
-        RaycastHit2D hit = Physics2D.Raycast(position, -position.normalized);
+		FireAbleObject[] fireObjects = GameObject.FindObjectsOfType<FireAbleObject>();
+		foreach (FireAbleObject fire in fireObjects)
+		{
+			if (Vector2.Distance(fire.transform.position, hit.point) < 100)
+				fire.StartFire();
+		}
 
-        FireAbleObject[] fireObjects = GameObject.FindObjectsOfType<FireAbleObject>();
-        foreach(FireAbleObject fire in fireObjects)
-        {
-            if(Vector2.Distance(fire.transform.position, hit.point) < 100)
-                fire.StartFire();
-        }
+		firePreview.gameObject.SetActive(false);
 
-        firePreview.gameObject.SetActive(false);
+		return null;
+	}
 
-        return null;
-    }
+	override public void StartPlacing()
+	{
+		base.StartPlacing();
 
-    override public void StartPlacing()
-    {
-        base.StartPlacing();
+		firePreview.transform.parent = null;
+		firePreview.gameObject.SetActive(true);
+	}
 
-        firePreview.transform.parent = null;
-        firePreview.gameObject.SetActive(true);
-    }
+	void Update()
+	{
+		if (Input.GetMouseButtonUp(0))
+		{
+			firePreview.gameObject.SetActive(false);
+		}
 
-    void Update()
-    {
-        if(Input.GetMouseButtonUp(0))
-        {
-            firePreview.gameObject.SetActive(false);
-        }
+		if (firePreview.gameObject.activeInHierarchy)
+		{
+			Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			firePreview.transform.position = pos.normalized * 365;
+		}
 
-        if(firePreview.gameObject.activeInHierarchy)
-        {
-            Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            firePreview.transform.position = pos.normalized * 365;
-        }
-        
-    }
+	}
 }
--- Assets/Scripts/PowerBase.cs
+++ Assets/Scripts/PowerBase.cs
@@ -4,11 +4,12 @@ public abstract class PowerBase : MonoBehaviour
 {
 	public virtual bool CanUse { get { return RemainingUses == -1 || RemainingUses > 0; } }
 	public abstract int RemainingUses { get; }
+	public abstract PowerManager.Powers Power { get; }
 
 	public virtual bool CanPlace(Vector2 position) { return CanUse; }
 
-    public virtual void StartPlacing() { }
-    public virtual void Cancel() { }
+	public virtual void StartPlacing() { }
+	public virtual void Cancel() { }
 
-    public abstract GameObject Create(Vector2 position);
+	public abstract GameObject Create(Vector2 position);
 }
