1aeffa5879d474f218d9a9ee646c6fa6a3ba24e1
Richard <rail.rich@gmail.com>
2017-04-23 15:16:43 -0400

    Planet splosion

--- Assets/Scripts/Emitters/PlanetSplosionEmitter.cs
+++ Assets/Scripts/Emitters/PlanetSplosionEmitter.cs
@@ -7,7 +7,8 @@ public class PlanetSplosionEmitter : BurstEmitter<GravityParticle> {
 	public override GravityParticle Spawn()
 	{
 		var particle = base.Spawn();
-		particle.Initialize(this, transform.position, new Vector2(Random.Range(-360, 360), Random.Range(-360, 360)));
+		particle.Initialize(this, transform.position, new Vector2(Random.Range(-360, 360), Random.Range(-360, 360))
+		,lifeTime:15f, friction:1f);
 
 		return particle;
 	}
