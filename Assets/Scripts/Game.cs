bef77731251b5e2f2def9da47edd866b8810cf7a
Magicolo <straty91@hotmail.com>
2017-04-22 22:10:32 -0400

    retry

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Game : MonoBehaviour
+{
+	public static Game Instance { get; private set; }
+
+	void Awake()
+	{
+		Instance = this;
+	}
+
+	public void Reload()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+}
